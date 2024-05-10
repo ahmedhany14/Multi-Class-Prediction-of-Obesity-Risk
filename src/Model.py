c80f16fe936033739186763e0dd7f1a56cede124
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-05-10 15:42:50 +0300

    models

--- src/Model.py
+++ src/Model.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 
 from sklearn.base import BaseEstimator, TransformerMixin
+
 # machine learning models
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -48,7 +49,105 @@ from tensorflow.keras.activations import relu, softmax, linear
 
 from keras_tuner import RandomSearch, Hyperband, BayesianOptimization
 
+import Processing as pr
 import pickle
 
-preprocessing = pickle.load(open("models_processing_pipeline.pkl", "rb"))
+fc = pr.Feature_Construction()
+enc = pr.Encode()
 
+
+def RF(X, y, fc, en):
+    rf = RandomForestClassifier(
+        n_estimators=250,
+        ccp_alpha=0.00015,
+        random_state=42,
+    )
+    pip_rf = Pipeline(
+        [("Feature_Construction", fc), ("Encode", en), ("RandomForestClassifier", rf)]
+    )
+
+    pip_rf.fit(X, y)
+    return pip_rf
+
+
+def XGB(X, y, fc, en):
+    xgb = XGBClassifier(n_estimators=300, learning_rate=0.05, max_depth=8)
+    leb_enc = LabelEncoder()
+    y = leb_enc.fit_transform(y)
+    pip_xgb = Pipeline(
+        [("Feature_Construction", fc), ("Encode", en), ("XGBClassifier", xgb)]
+    )
+
+    pip_xgb.fit(X, y)
+    return pip_xgb
+
+
+def LGBM(X, y, fc, en):
+    lgbm = LGBMClassifier(
+        objective="multiclass",
+        boosting_type="gbdt",
+        num_class=7,
+        learning_rate=0.01,
+        n_estimators=500,
+        max_depth=11,
+    )
+    pip_lgbm = Pipeline(
+        [("Feature_Construction", fc), ("Encode", en), ("LGBMClassifier", lgbm)]
+    )
+
+    pip_lgbm.fit(X, y)
+    return pip_lgbm
+
+def vot(X, y):
+    rf = RF(X, y, fc, enc)
+    xgb = XGB(X, y, fc, enc)
+    lgbm = LGBM(X, y, fc, enc)
+    
+    vot_system = VotingClassifier(
+    estimators=[
+            ("random", rf),
+            ("xgbc", xgb),
+            ("pip_lgbm", lgbm),
+        ],
+    )
+    vot_system.fit(X, y)
+    return vot_system
+
+
+def save_model(model):
+    with open("voting_classifier.pkl", "wb") as f:
+        pickle.dump(model, f)
+        
+
+data = pd.read_csv("/home/ahmed/Ai/Data science and Ml projects/Multi-Class-Prediction-of-Obesity-Risk/Data set/train.csv")
+
+def rename_columns(data, names):
+    data.rename(columns=names, inplace=True)
+    return data
+
+columns_names = {
+    "Gender": 'gender',
+    'Age':"age",
+    'Height':"height",
+    'Weight':"weight",
+    'family_history_with_overweight':"family_history",
+    'FAVC':"frequency_high_caloric_food",
+    'FCVC':"frequency_vegetables",
+    'NCP':"main_meals",
+    'CAEC':"eating_out_main_meals",
+    'SMOKE':"smoking",
+    'CH2O':"water_daily",
+    'SCC':"calories_monitoring",
+    'FAF':"physical_activity",
+    'TUE':"technology_use",
+    'CALC':"alcohol",
+    'MTRANS':"transportation",
+    'NObeyesdad':"obesity"
+}
+
+train_data = rename_columns(data, columns_names)
+
+X = data.drop("obesity", axis=1)
+y = data["obesity"]
+
+save_model(vot(X, y))
